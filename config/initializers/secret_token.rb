e0e4b63b0223193470ab9a7a72a037fe886347ac
juanito15 <juanisantacruz@gmail.com>
2014-02-23 21:01:46 +0000

    scraper

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sky2::Application.config.secret_key_base = 'e741ba5449e4fe11863f9a6462a8447c8cd917f31e593aed91a0288cfaf1b894fe8e749c5c470046ac8c5427bc88c325c2c4ee66481e2a8c5b157211a97498f1'
